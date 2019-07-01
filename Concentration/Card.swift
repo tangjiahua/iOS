221a03b235a388a839489a28e06c04e653aa6046
Jiahua <tangjiahua@bit.edu.cn>
2019-07-01 22:50:37 +0800

    my concentration program
    learned from CS193P of Stanford

---
+++ Concentration/Card.swift
@@ -0,0 +1,27 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by 汤佳桦 on 2019/6/29.
+//  Copyright © 2019 BIT. All rights reserved.
+//
+
+import Foundation
+//No inherit
+//Are value types, provide copy; class is reference type,pointers, stacks
+struct Card{
+    var isFaceup = false
+    var isMactched = false
+    var identifier: Int
+    
+    static var identifierFactory = 0
+    
+    static func getUniqueIdentifier() -> Int{
+        identifierFactory += 1//because of static identifierfactory
+        return Card.identifierFactory
+    }
+    
+    init(){
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
---
+++ Concentration/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by 汤佳桦 on 2019/6/27.
+//  Copyright © 2019 BIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    lazy var game: Concentration = Concentration(numberOfPairsOfCards: cardButtons.count / 2)
+    
+    var flipCount = 0{
+        didSet{
+             CounterLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var CounterLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender){
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
+        }else{
+            print("chosen card was not in cardBUttons")
+        }
+        
+    }
+    
+    func updateViewFromModel(){
+        for index in cardButtons.indices{
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceup{
+                button.setTitle(emoji(for: card), for: UIControl.State.normal)
+                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            }
+            else{
+                button.setTitle("", for: UIControl.State.normal)
+                button.backgroundColor = card.isMactched ? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+            }
+        }
+    }
+    
+    var emojiChoice = ["🧧", "🎃", "👻", "🥰","🐭","🕷", "🐞", "🏀","🐵"]
+    
+    
+    var emoji = [Int: String]()
+    
+    func emoji(for card: Card) -> String {
+        if emoji[card.identifier] == nil, emojiChoice.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoice.count)))
+            emoji[card.identifier] = emojiChoice.remove(at: randomIndex)
+        }
+        return emoji[card.identifier] ?? "?"
+    }
+}
